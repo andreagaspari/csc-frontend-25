1b8e2ad4ffdfcb03d7c4afcee1276c64d4391b47
Andrea Gaspari <andrea.gaspari2@gmail.com>
2025-11-18 11:15:34 +0100

    Aggiungi il file esercitazione-01.php con esercizi su operazioni matematiche e verifica di uguaglianza

---
+++ 02 - Linguaggio PHP/esercitazioni/esercitazione-01.php
@@ -0,0 +1,28 @@
+<?php
+
+    /**
+     * Es 01
+     * Dati due numeri, stampare nell'ordine:
+     *  - Somma
+     *  - Differenza
+     *  - Moltiplicazione
+     *  - Divisione
+     */
+    $a = 12;
+    $b = 24;
+
+
+    /**
+     * Es 02
+     * Data una parola, verificare se è uguale
+     * a "casa"
+     */
+    $parola = "albero";
+
+
+    /** 
+     * Es 03
+     * Dato due valori numerici, verificarne l'uguaglianza
+     */
+    $c = "145";
+    $d = 145;
